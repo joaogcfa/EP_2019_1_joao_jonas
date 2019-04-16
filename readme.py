f4ba12d27ff4b9ec522c41292566b500a7830d6c
jonaslopes19 <jonasl1@insper.al.edu.br>
2019-04-16 17:26:24 -0300

    tchutchuca é mãe e a vó

---
+++ readme.py
@@ -0,0 +1,7 @@
+Exercício Programa - 2019/1 - Design de Software
+------------------------------------------------
+
+Alunos: 
+- Jonas da Silva Lopes, jonasl1@al.insper.edu.br
+- João Guilherme Almeida, joaogcfa@al.insper.edu.br
+
