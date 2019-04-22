f1c904786f66f440d11629ae0eb89b43f54f327e
Joao Almeida <joaoalmeida@Joaos-MacBook-Pro-2.local>
2019-04-21 23:16:58 -0300

    pf n de errado

--- Arquivodoprograma(1).py
+++ Arquivodoprograma(1).py
@@ -5,27 +5,27 @@ def carregar_cenarios():
             "descricao": "Voce esta no saguao de entrada do prédio um do insper",
             "opcoes": {
                 "casa do pao de queijo": "Ir até  casa do pão queijo, talvez comer algo?",
-                "Prédio novo": "Ir de patinete da grin para rumo ao prédio de engenharia",
+                "predio novo": "Ir de patinete da grin para rumo ao prédio de engenharia",
 				'O grande elevador': 'sempre demorado, porém sempre nos leva instantaneamente'
             }
         },
-        "Prédio novo": {
+        "predio novo": {
             "titulo": "Onde a felicidade começa",
             "descricao": "Saguão do prédio novo:Onde todos entram e acham que serão felizes",
             "opcoes": {
                 "inicio": "Voltar de patinete da yellow para o prédio antigo",
-                "Fab lab": "Pegar elevador para ir ao fab lab",
+                "fab lab": "Pegar elevador para ir ao fab lab",
 				'segundo andar':'subir de escada para o segundo andar'
             }
         },
-        "Fab lab": {
+        "fab lab": {
             "titulo": "Os QUATRO terrores",
             "descricao": "Um dos técnicos te fará vencer, já os outros...",
-            "opcoes": {"Técnico da cortadora a laser": "Pra que a cortadora a laser serve?", 
-					   'Técnico da fresadora':'O que a fresadora faz? ',
-					   'Técnico de solda':'O que se aprende no job rotation de solda?', 
-					   'Técnico da impressora 3d': 'O que se pode fazer com uma impressora 3d?', 
-					   'Prédio novo':'Pegar elevador para o térreo do prédio'}
+            "opcoes": {"tecnico da cortadora a laser": "Pra que a cortadora a laser serve?", 
+					   'tecnico da fresadora':'O que a fresadora faz? ',
+					   'tecnico de solda':'O que se aprende no job rotation de solda?', 
+					   'tecnico da impressora 3d': 'O que se pode fazer com uma impressora 3d?', 
+					   'predio novo':'Pegar elevador para o térreo do prédio'}
         },
         'casa do pao de queijo': {
             "titulo": "O cafezin da sorte",
@@ -35,13 +35,13 @@ def carregar_cenarios():
 				'comprar pao de queijo':'Você tem dinheiro pra isso?'
             }
         },
-		'Segundo andar': {
+		'segundo andar': {
 			'titulo': 'A hora da verdade',
 			'descrição':'Acho que o professor está aqui',
-			'opções':{'Prédio novo':'pegar elevador para o saguão do prédio de engenharia',
+			'opções':{'predio novo':'pegar elevador para o saguão do prédio de engenharia',
 					  'descer no tobogã':'você consegue fazer isso?',
 					  'sala do professor':'caminhe lentamente até a sala onde ele se encontra',
-					  'Fab lab':'va de elevador ao Fab lab'
+					  'fab lab':'va de elevador ao Fab lab'
 			}
 				},
 		'sala do professor': {
@@ -49,22 +49,22 @@ def carregar_cenarios():
 			'desrição':'faça alguma coisa, ou vai arregar?',
 			'opções':{'segundo andar':'Arregue agora',
 			          'assuste o professor':'faça uma bricadeirinha sadia com ele',
-					  'vá falar com o professor':'agora é a hora da verdade'}
+					  'va falar com o professor':'agora é a hora da verdade'}
 			   },
         "comprar pao de queijo": {
             "titulo": "Hora da batalha",
             "descricao": "Quando você foi comprar seu pão de queijo, você irritou a caixa e agora ela está te desafiando para uma batalha 1 contra 1",
             "opcoes": {
                 "incio": "Você é um medroso que fugirá de suas batalhas e voltará para o saguão de entrada",
-                "Lutar": "Iniciar sua batlha",
+                "lutar": "Iniciar sua batlha",
             }
         },
-        "Lutar": {
+        "lutar": {
             "titulo": "Sua batalha começou",
-            "descricao": "Você: 100 hit points, 15 pontos de ataque e 10 pontos de defesa"
+            "descricao": "Você: 100 hit points, 15 pontos de ataque e 10 pontos de defesa\n"
             "Caixa: 10 hit points, 5 pontos de ataque e 5 pontos de defesa",
             "opcoes": {
-                "atacar caixa": "Atacar o seu oponente",
+                "atacar caixa": "atacar o seu oponente",
                 "fugir": "última chance de fugir",
             }
         },
@@ -72,7 +72,7 @@ def carregar_cenarios():
             "titulo": "Doce Vitória",
             "descricao": "Parabéns, você venceu! Como forma de desculpa pelo caixa louco, a casa do Pão de queijo te deu o ataque do café quente, use sabiamente",
             "opcoes": {
-                "inicio": "Voltar para o saguão de entrada",
+                "inicio": "voltar para o saguão de entrada",
             }
         },
 		'O grande elevador':{
@@ -82,7 +82,53 @@ def carregar_cenarios():
 				'responder a pergunta':'teleporte-se para onde quiser',
 				'voltar ao saguão': 'Estas a perder uma grande oportunidade'	
 					}
-				}
+				},
+        'tecnico da fresadora': {
+            "titulo": "O técnico assassino",
+            "descricao": "você escolheu pobreamente, o que te fez cair em ruina",
+            "opcoes": {
+                "acabaram as opções":"não há nada para se fazer"
+            }
+        },
+        'tecnico da solda': {
+            "titulo": "O técnico assassino",
+            "descricao": "você escolheu pobreamente, o que te fez cair em ruina",
+            "opcoes": {
+                "acabaram as opções":"não há nada para se fazer"
+            }
+        },
+        'tecnico da cortadora laser': {
+            "titulo": "",
+            "descricao": "você escolheu pobreamente, o que te fez cair em ruina",
+            "opcoes": {
+                "acabaram as opções":"não há nada para se fazer"
+            }
+        },
+        'tecnico da impressora 3d': {
+            "titulo": "O técnico maluco",
+            "descricao": "O técnico escolhido ficou indignado que vc não consguiu fazer o EP para hoje e está te desafiando a uma batalha para lhe dar uma lição",
+            "opcoes": {
+                "predio novo":"você ficou com medo do que ele poderia fazer e quais armas ele poderia ter feito na impressora 3D",
+                "lutar com o tecnico":"desafiar o tecnico"
+            }
+        },
+        'lutar com o tecnico': {
+            "titulo": "A batalha",
+            "descricao": "Você: 100 hit points, 15 pontos de ataque e 10 pontos de defesa\n"
+            "Caixa: 1000 hit points, 25 pontos de ataque e 25 pontos de defesa",
+            "opcoes": {
+                "predio novo":"você ficou com medo do que ele poderia fazer",
+                "atacar o tecnico":"desafiar o tecnico",
+                "ataque do cafe quente": "usar o ataque do café quente"
+            }
+        },
+        'atacar o tecnico': {
+            "titulo": "",
+            "descricao": "você escolheu pobreamente, o que te fez cair em ruina",
+            "opcoes": {
+                "acabaram as opções":"não há nada para se fazer"
+            }
+        },    
 	}
     nome_cenario_atual = "inicio"
     return cenarios, nome_cenario_atual
@@ -112,7 +158,30 @@ def main():
         if nome_cenario_atual == "atacar caixa":
             print('Você saiu na porrada com a atendente e não saiu no prejú')
             itens.append("ataque do café quente")
-			
+        if nome_cenario_atual == "tecnico da fresadora":
+            print()
+            print("Você escolheu probreamente, já que o técnico era na verdade um vilão que te matou com a fresadora")
+            print()
+            game_over = True
+            break
+        if nome_cenario_atual == "tecnico da solda":
+            print()
+            print("Você escolheu probreamente, já que o técnico era na verdade um vilão que te matou com a solda")
+            print()
+            game_over = True
+            break
+        if nome_cenario_atual == "tecnico da cortadora a laser":
+            print()
+            print("Você escolheu probreamente, já que o técnico era na verdade um vilão que te matou com a cortadora laiser")
+            print()
+            game_over = True
+            break
+        if nome_cenario_atual == "tecnico da cortadora a laser":
+            print()
+            print("Você escolheu probreamente, já que o técnico era na verdade um vilão que te matou com a cortadora laiser")
+            print()
+            game_over = True
+            break        
 #parte do teleporte, dá uma verificada se vai, também adicionei o cenário do elevador			
         elif cenario_atual == 'O grande elevador':
             for opção in cenario_atual['opcoes']:
@@ -128,7 +197,10 @@ def main():
                 print(sala)
                 print('-'*len(sala))
                 print()
-                print("você tem esses itens:",itens)
+                print("voce tem esses itens:", end = '')
+                for i in itens:
+                    print(i, end = '')
+                print()
                 for descricao,texto in cenario_atual.items():
                     if descricao == 'descricao':
                         print(texto)
